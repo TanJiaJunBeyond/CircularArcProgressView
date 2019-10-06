ca6122f16ab6a3246350376a779ce9f54dbd85a1
TanJiaJun <1120571286@qq.com>
2019-10-07 02:11:38 +0800

    新建项目。

--- app/src/androidTest/java/com/tanjiajun/circulararcprogressview/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tanjiajun/circulararcprogressview/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
-package com.tanjiajun.circulararcprogressviewdemo
+package com.tanjiajun.circulararcprogressview
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.tanjiajun.circulararcprogressviewdemo", appContext.packageName)
+        assertEquals("com.tanjiajun.circulararcprogressview", appContext.packageName)
     }
 }
